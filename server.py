70b677ff8f2440c9debb03f71008160279ea447c
Michael Ebenstein <mebenstein@student.tgm.ac.at>
2019-04-16 10:46:15 +0200

    add add rest

--- server.py
+++ server.py
@@ -1,27 +1,41 @@
-from flask import Flask
+from flask import Flask,request
 from flask_sqlalchemy import SQLAlchemy
-from threading import Lock
+import json
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://todo.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todos.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
 class ToDoObject(db.Model):
-    lock = Lock()
+    __tablename__ = "todos"
     id = db.Column(db.Integer, primary_key=True)
     locked = False
-    title = db.Column(db.String(80))
-    parent = db.Column(db.Integer,db.ForeignKey('todoobject.id'))
+    title = db.Column(db.String(80),nullable=False)
+    parent_id = db.Column(db.Integer,db.ForeignKey('todos.id'))
+    children = db.relationship('ToDoObject', backref=db.backref('parent',remote_side=[id]))
 
-    def __init__(self,title):
+    def __init__(self,title,parent=None):
         self.title = title
-
-        with lock:
-            self.id = ToDoObject.id
-            ToDoObject.id += 1
+        self.parent_id = parent
 
     def setLocked(self,state):
         self.locked = state
 
+db.create_all()
+db.session.commit()
 
+@app.route("/add")
+def addTodo():
+    obj = json.loads(request.data)
+    
+    todo = ToDoObject(obj["title"],obj.get("parent"))
 
+    db.session.add(todo)
+    db.session.commit()
+
+    return (str(todo.id),0)
+
+if __name__ == "__main__":
+    app.run(port=9000)
